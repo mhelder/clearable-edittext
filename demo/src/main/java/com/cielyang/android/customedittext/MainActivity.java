acf7204e8a2264838b539c9b81f1760f70cfbda2
mhelder <matthias.helder@justpinch.com>
2017-11-10 18:01:19 +0100

    + add on clear listener to sample

--- demo/src/main/java/com/cielyang/android/customedittext/MainActivity.java
+++ demo/src/main/java/com/cielyang/android/customedittext/MainActivity.java
@@ -2,12 +2,28 @@ package com.cielyang.android.customedittext;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.cielyang.android.clearableedittext.ClearableAutoCompleteTextView;
+import com.cielyang.android.clearableedittext.ClearableEditText;
+import com.cielyang.android.clearableedittext.OnClearListener;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((ClearableEditText) findViewById(R.id.edit_text)).setOnClearListener(new OnClearListener() {
+            @Override public void onCleared() {
+                Toast.makeText(MainActivity.this, "EditText cleared", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ((ClearableAutoCompleteTextView) findViewById(R.id.autocomplete_text_view)).setOnClearListener(new OnClearListener() {
+            @Override public void onCleared() {
+                Toast.makeText(MainActivity.this, "AutoCompleteTextView cleared", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
